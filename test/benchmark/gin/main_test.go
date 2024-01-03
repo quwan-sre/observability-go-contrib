09783e2ff7f6b970e42442ff357ed05926f738c1
Jiekun <zhujiekun@52tt.com>
2024-01-03 11:58:23 +0800

    feat: [gin] Added benchmark

---
+++ test/benchmark/gin/main_test.go
@@ -0,0 +1,55 @@
+package gin
+
+import (
+	"fmt"
+	"github.com/go-resty/resty/v2"
+	"net/http"
+	"testing"
+	"time"
+)
+
+var (
+	client *resty.Client
+)
+
+func TestMain(t *testing.M) {
+	// prepare a gin HTTP server
+	go main()
+
+	// health check
+	for {
+		resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%d/health", httpPort))
+		if err != nil || resp.StatusCode != 200 {
+			time.Sleep(1 * time.Second)
+		} else {
+			break
+		}
+	}
+
+	for {
+		resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%d/health", httpInstrumentPort))
+		if err != nil || resp.StatusCode != 200 {
+			time.Sleep(1 * time.Second)
+		} else {
+			break
+		}
+	}
+
+	client = resty.New()
+
+	// ready, run the test case
+	t.Run()
+}
+
+func BenchmarkGin(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		client.R().Get(fmt.Sprintf("http://127.0.0.1:%d/health", httpPort))
+	}
+
+}
+
+func BenchmarkGinWithInstrument(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		client.R().Get(fmt.Sprintf("http://127.0.0.1:%d/health", httpInstrumentPort))
+	}
+}
