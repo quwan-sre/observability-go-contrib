3ca697dd51d390898909c3ef070c222724012681
Jiekun <jiekun.dev@gmail.com>
2024-01-04 18:36:54 +0800

    chore: [init] Added cornjob reset and remove useless metrics (#6)

--- metrics/gin/gin.go
+++ metrics/gin/gin.go
@@ -15,30 +15,32 @@ func NewMetricsMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		// before
 		startTime := time.Now()
+		defer func() {
+			latency := time.Since(startTime)
+			endpoint := ""
+			responseCode := -1
 
+			if ctx != nil && ctx.Request != nil && ctx.Request.URL != nil {
+				endpoint = ctx.Request.URL.Path
+			}
+
+			if ctx != nil && ctx.Writer != nil {
+				responseCode = ctx.Writer.Status()
+			}
+
+			common.DefaultRPCReceiveRequestMetric.With(prometheus.Labels{
+				"sdk":              common.RPCSDKGin,
+				"request_protocol": common.RPCProtocolHTTP,
+				"endpoint":         endpoint,
+				"rpc_status_code":  strconv.Itoa(int(grpc.OK)),
+				"http_status_code": strconv.Itoa(responseCode),
+			}).Observe(latency.Seconds())
+		}()
 		// execute
 		ctx.Next()
 
 		// after
-		latency := time.Since(startTime)
-		endpoint := ""
-		responseCode := -1
 
-		if ctx != nil && ctx.Request != nil && ctx.Request.URL != nil {
-			endpoint = ctx.Request.URL.Path
-		}
-
-		if ctx != nil && ctx.Writer != nil {
-			responseCode = ctx.Writer.Status()
-		}
-
-		common.DefaultRPCReceiveRequestMetric.With(prometheus.Labels{
-			"sdk":              common.RPCSDKGin,
-			"request_protocol": common.RPCProtocolHTTP,
-			"endpoint":         endpoint,
-			"rpc_status_code":  strconv.Itoa(int(grpc.OK)),
-			"http_status_code": strconv.Itoa(responseCode),
-		}).Observe(latency.Seconds())
 		return
 	}
 }
