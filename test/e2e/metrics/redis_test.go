df63e76769075896e3984ce4816e0776f0540f43
Jiekun <zhujiekun@52tt.com>
2024-01-09 12:58:53 +0800

    feat: [go-redis] enable go redis check without health check

--- test/e2e/metrics/redis_test.go
+++ test/e2e/metrics/redis_test.go
@@ -1,11 +1,13 @@
 package metrics
 
 import (
+	"context"
 	"fmt"
 	"github.com/go-redis/redis/v9"
 	go_redis "github.com/quwan-sre/observability-go-contrib/metrics/go-redis"
 	"os"
 	"strings"
+	"testing"
 )
 
 var (
@@ -28,39 +30,39 @@ func initRedisClient() {
 		Addrs: []string{strings.Join([]string{redisHost, redisPort}, ":")},
 	})
 	redisClient.AddHook(go_redis.NewRedisHook([]string{strings.Join([]string{redisHost, redisPort}, ":")}))
-
+	return
 }
 
-//func TestRedisGo(t *testing.T) {
-//	initRedisClient()
-//
-//	health := false
-//	for i := 0; i < 20; i++ {
-//		if redisClient.Ping(context.TODO()).Err() != nil {
-//			fmt.Println("redis health check")
-//			time.Sleep(time.Second)
-//		} else {
-//			health = true
-//			break
-//		}
-//	}
-//
-//	if !health {
-//		t.Fatalf("redis not ready")
-//	}
-//
-//	if redisClient.Set(context.TODO(), "foo", "bar", 0).Err() != nil {
-//		t.Error("redis err")
-//	}
-//	redisClient.Get(context.TODO(), "hello")
-//	redisClient.Do(context.TODO(), "not_exist_command", "invalid_value")
-//
-//	pipe := redisClient.Pipeline()
-//	pipe.Del(context.TODO(), "foo")
-//	pipe.Get(context.TODO(), "foo")
-//	pipe.Del(context.TODO(), "foo")
-//	pipe.Do(context.TODO(), "not_exist_command", "invalid_value")
-//	pipe.Exec(context.TODO())
-//
-//	return
-//}
+func TestRedisGo(t *testing.T) {
+	initRedisClient()
+
+	//health := false
+	//for i := 0; i < 20; i++ {
+	//	if redisClient.Ping(context.TODO()).Err() != nil {
+	//		fmt.Println("redis health check")
+	//		time.Sleep(time.Second)
+	//	} else {
+	//		health = true
+	//		break
+	//	}
+	//}
+	//
+	//if !health {
+	//	t.Fatalf("redis not ready")
+	//}
+
+	if redisClient.Set(context.TODO(), "foo", "bar", 0).Err() != nil {
+		t.Error("redis err")
+	}
+	redisClient.Get(context.TODO(), "hello")
+	redisClient.Do(context.TODO(), "not_exist_command", "invalid_value")
+
+	pipe := redisClient.Pipeline()
+	pipe.Del(context.TODO(), "foo")
+	pipe.Get(context.TODO(), "foo")
+	pipe.Del(context.TODO(), "foo")
+	pipe.Do(context.TODO(), "not_exist_command", "invalid_value")
+	pipe.Exec(context.TODO())
+
+	return
+}
